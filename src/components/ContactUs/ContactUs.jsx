24e8e91bf6ec99c97e5c0b0caec4b8c9b3f442ba
obafemi <oludahunsiobafemi18@gmail.com>
2022-06-01 15:55:38 +0100

    completed the UI design for the contact-us page.

---
+++ src/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styles from "./ContactUs.module.css";
+import { ReactComponent as Arrow } from "../../assets/arrow-right-solid.svg";
+
+const ContactUs = ({ close }) => {
+  return (
+    <div className={styles["page-container"]}>
+      <div className={styles.title}>
+        Hey, we are still in the works, <br /> but you can send us a message!
+      </div>
+      <div className={styles["input-section"]}>
+        <div className={styles.form}>
+          <label className={styles.label} htmlFor="firstName">
+            {" "}
+            First name{" "}
+          </label>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Enter your first name"
+          />
+        </div>
+        <div className={styles.form}>
+          <label className={styles.label} htmlFor="firstName">
+            {" "}
+            Last name{" "}
+          </label>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Enter your last name"
+          />
+        </div>
+        <div className={styles.form}>
+          <label className={styles.label} htmlFor="firstName">
+            {" "}
+            Email address{" "}
+          </label>
+          <input
+            className={styles.input}
+            type="email"
+            placeholder="Enter your Email Address"
+          />
+        </div>
+        <div className={styles.form}>
+          <label className={styles.label} htmlFor="firstName">
+            {" "}
+            Tell Us What You Need Help With:{" "}
+          </label>
+          <textarea
+            style={{ height: "130px", paddingTop: "5px" }}
+            className={styles.input}
+            type="text"
+            placeholder="Enter 
+            A Topic, Like 'Channel Problem...' "
+          />
+        </div>
+      </div>
+      <div className={styles.cta}>
+        <button className={styles.btn}> SEND NOW </button>
+      </div>
+      <div onClick={close} className={styles["close-btn"]}>
+        <Arrow height={20} width={20} fill="blue" />
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
--- src/pages/ComingSoon/ComingSoon.jsx
+++ src/pages/ComingSoon/ComingSoon.jsx
@@ -6,11 +6,15 @@ import { ReactComponent as Facebook } from "../../assets/facebook-square-brands.
 import { ReactComponent as Instagram } from "../../assets/instagram-brands.svg";
 import { ReactComponent as LinkedIn } from "../../assets/linkedin-brands.svg";
 import { ReactComponent as Twitter } from "../../assets/twitter-square-brands.svg";
+import ContactUs from "../../components/ContactUs/ContactUs";
 
 const ComingSoon = () => {
   const [isAnimate, setIsAnimate] = useState(false);
   const handleContactUs = () => {
-    setIsAnimate(!isAnimate);
+    setIsAnimate(true);
+  };
+  const closeContactUs = () => {
+    setIsAnimate(false);
   };
   const countdown = [
     {
@@ -35,114 +39,127 @@ const ComingSoon = () => {
     },
   ];
   return (
-    <div
-      className={isAnimate ? `${styles.blur}` : `${styles["page-container"]}`}
-    >
-      <NavBar contactUs={handleContactUs} />
-      <div className={styles["big-sphere"]} />
-      <div className={styles["medium-sphere"]} />
-      <div className={styles["small-sphere"]} />
-      <div className={styles["page-content"]}>
-        <div className={isAnimate ? `${styles.lowercase}` : `${styles.header}`}>
-          {" "}
-          SOMETHING AWESOME IS <br /> COMING SOON{" "}
-        </div>
-        <div className={styles["sub-header"]}>
-          {" "}
-          Yout all-in-one affiliate marketing tracking software <b>
-            track
-          </b>, <b>automate</b> and <br /> <b>optimize</b> your campaigns{" "}
-        </div>
-        <div className={styles.counter}>
-          {countdown.map((cntdwn) => (
-            <div className={styles["count-box"]}>
-              <div className={styles["count-value"]}>{cntdwn.value}</div>
-              <div className={styles["count-title"]}> {cntdwn.title} </div>
-            </div>
-          ))}
-        </div>
-        <div className={styles["form-container"]}>
+    <div className={styles.container}>
+      <div
+        className={isAnimate ? `${styles.blur}` : `${styles["page-container"]}`}
+      >
+        <NavBar contactUs={handleContactUs} />
+        <div className={styles["big-sphere"]} />
+        <div className={styles["medium-sphere"]} />
+        <div className={styles["small-sphere"]} />
+        <div className={styles["page-content"]}>
           <div
-            className={
-              isAnimate ? `${styles.display}` : `${styles["input-container"]}`
-            }
+            className={isAnimate ? `${styles.lowercase}` : `${styles.header}`}
           >
-            <div className={styles.form}>
-              <input
-                className={styles["name-input"]}
-                type="text"
-                name="firstName"
-              />
-              <label htmlFor="firstName"> First Name </label>
-            </div>
-            <div className={styles.form}>
-              <input
-                className={styles["name-input"]}
-                type="text"
-                name="lastName"
-              />
-              <label htmlFor="lastName"> Last Name </label>
-            </div>
+            {" "}
+            SOMETHING AWESOME IS <br /> COMING SOON{" "}
           </div>
-          <div
-            className={
-              isAnimate ? `${styles["move-up"]}` : `${styles["email-section"]}`
-            }
-          >
-            <input className={styles["email-input"]} type="email" />
-            <button
+          <div className={styles["sub-header"]}>
+            {" "}
+            Yout all-in-one affiliate marketing tracking software <b>
+              track
+            </b>, <b>automate</b> and <br /> <b>optimize</b> your campaigns{" "}
+          </div>
+          <div className={styles.counter}>
+            {countdown.map((cntdwn) => (
+              <div className={styles["count-box"]}>
+                <div className={styles["count-value"]}>{cntdwn.value}</div>
+                <div className={styles["count-title"]}> {cntdwn.title} </div>
+              </div>
+            ))}
+          </div>
+          <div className={styles["form-container"]}>
+            <div
               className={
-                isAnimate
-                  ? `${styles["btn-move-up"]}`
-                  : `${styles["email-btn"]}`
+                isAnimate ? `${styles.display}` : `${styles["input-container"]}`
               }
             >
-              JOIN OUR WAITING LIST
-            </button>
+              <div className={styles.form}>
+                <input
+                  className={`${styles["name-input"]} ${styles.input} `}
+                  type="text"
+                  name="firstName"
+                />
+                <label className={styles.label} htmlFor="firstName">
+                  {" "}
+                  First Name{" "}
+                </label>
+              </div>
+              <div className={styles.form}>
+                <input
+                  className={`${styles["name-input"]} ${styles.input} `}
+                  type="text"
+                  name="lastName"
+                />
+                <label className={styles.label} htmlFor="lastName">
+                  {" "}
+                  Last Name{" "}
+                </label>
+              </div>
+            </div>
+            <div
+              className={
+                isAnimate
+                  ? `${styles["move-up"]}`
+                  : `${styles["email-section"]}`
+              }
+            >
+              <input className={styles["email-input"]} type="email" />
+              <button
+                className={
+                  isAnimate
+                    ? `${styles["btn-move-up"]}`
+                    : `${styles["email-btn"]}`
+                }
+              >
+                JOIN OUR WAITING LIST
+              </button>
+            </div>
           </div>
-        </div>
-        <div className={styles.footer}>
-          <div className={styles["socials-icon"]}>
-            <Youtube
-              className={styles.icon}
-              height={20}
-              width={20}
-              fill="white"
-            />
-            <Facebook
-              className={styles.icon}
-              height={20}
-              width={20}
-              fill="white"
-            />
-            <LinkedIn
-              className={styles.icon}
-              height={20}
-              width={20}
-              fill="white"
-            />
-            <Instagram
-              className={styles.icon}
-              height={20}
-              width={20}
-              fill="white"
-            />
-            <Twitter
-              className={styles.icon}
-              height={20}
-              width={20}
-              fill="white"
-            />
-          </div>
-          <div className={styles.links}>
-            <p className={styles.link}>Terms of services</p>
-            <p className={styles.link}>Privacy policy</p>
-          </div>
-          <div className={styles.copyright}>
-            Copyright 2021 @ Peddle Technologies. All Rights Reserved.
+          <div className={styles.footer}>
+            <div className={styles["socials-icon"]}>
+              <Youtube
+                className={styles.icon}
+                height={20}
+                width={20}
+                fill="white"
+              />
+              <Facebook
+                className={styles.icon}
+                height={20}
+                width={20}
+                fill="white"
+              />
+              <LinkedIn
+                className={styles.icon}
+                height={20}
+                width={20}
+                fill="white"
+              />
+              <Instagram
+                className={styles.icon}
+                height={20}
+                width={20}
+                fill="white"
+              />
+              <Twitter
+                className={styles.icon}
+                height={20}
+                width={20}
+                fill="white"
+              />
+            </div>
+            <div className={styles.links}>
+              <p className={styles.link}>Terms of services</p>
+              <p className={styles.link}>Privacy policy</p>
+            </div>
+            <div className={styles.copyright}>
+              Copyright 2021 @ Peddle Technologies. All Rights Reserved.
+            </div>
           </div>
         </div>
       </div>
+      {isAnimate && <ContactUs close={closeContactUs} />}
     </div>
   );
 };
